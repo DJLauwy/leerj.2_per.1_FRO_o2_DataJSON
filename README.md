1c2404b48831a107d11baba9b8a9f917f28eb882
DJLauwy <laudehoop@hotmail.com>
2020-10-05 14:15:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# leerj.2_per.1_FRO_o2_DataJSON
+ Leerjaar 2 Periode 1 FRO opdracht2 Data met JSON
---
+++ boeken.js
@@ -0,0 +1,30 @@
+const uitvoer = document.getElementById('boeken');
+const xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = () => {
+	if(xhr.readyState == 4 && xhr.status == 200){
+		let resultaat = JSON.parse(xhr.responseText);
+		boeken.data = resultaat;
+		boeken.uitvoeren();
+	}
+}
+
+xhr.open('GET', 'boeken.json', true);
+xhr.send();
+
+const boeken = {
+	//er wordt hier een eigenschap data aangemaakt (regel 7)
+	uitvoeren(){
+		let html = "";
+		this.data.forEach(boek =>{
+			//in het geval van een voortitel moet deze voor de titel worden geplaatst
+			let titel = "";
+			if(boek.voortitel){
+				titel += boek.voortitel + " ";
+			}
+			titel += boek.titel;
+			html += `<h3>${titel}</h3>`;
+		});
+		uitvoer.innerHTML = html;
+	}
+}
